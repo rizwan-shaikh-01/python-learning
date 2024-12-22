d85c59c697dc36922873e105537cf923b6651934
rizwan-shaikh-01 <rizwanshaikhakhtar314@gmail.com>
2024-12-22 21:40:42 +0530

    test

--- Python-CWH/Day-10/calculator.py
+++ Python-CWH/Day-10/calculator.py
@@ -4,7 +4,4 @@ a = int(input("Enter a number : "))
 b = int(input("Enter a number : "))
 
 print(f"Addition of {a} + {b} = {a+b}")
-print(f"Subtraction of {a} - {b} = {a-b}")
-print(f"Multiplication of {a} * {b} = {a*b}")
-print(f"Division of {a} / {b} = {a/b}")
 
