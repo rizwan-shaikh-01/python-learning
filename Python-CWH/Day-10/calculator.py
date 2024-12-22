6da19dc47b695dfaf8435ad84c3dc3a75e54d2ac
rizwan-shaikh-01 <rizwanshaikhakhtar314@gmail.com>
2024-12-22 21:43:48 +0530

    Day-10 Sub, Mul, Div added in calculator.py

--- Python-CWH/Day-10/calculator.py
+++ Python-CWH/Day-10/calculator.py
@@ -4,4 +4,7 @@ a = int(input("Enter a number : "))
 b = int(input("Enter a number : "))
 
 print(f"Addition of {a} + {b} = {a+b}")
+print(f"Subtraction of {a} - {b} = {a-b}")
+print(f"Multiplication of {a} * {b} = {a*b}")
+print(f"Division of {a} / {b} = {a/b}")
 
